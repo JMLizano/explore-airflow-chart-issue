0e915148c4d4c75b7c5d8eddbebc9c348726f011
chema <josemaria.lizano@lynxanalytics.com>
2023-04-28 19:22:50 +0800

    change start time

--- dags/mypackage/mydags/mydag.py
+++ dags/mypackage/mydags/mydag.py
@@ -7,7 +7,7 @@ from mypackage.lib.mylib import lib_method
 
 with DAG(
     dag_id="my_dag_name",
-    start_date=datetime.datetime(2023, 4,28),
+    start_date=datetime.datetime(2023, 4,1),
     schedule="@daily",
 ):
     EmptyOperator(task_id="task")
