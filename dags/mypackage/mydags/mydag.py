2cf4086c3c8eb203c640213455121b4e07701364
chema <josemaria.lizano@lynxanalytics.com>
2023-04-28 19:31:14 +0800

    change start time

--- dags/mypackage/mydags/mydag.py
+++ dags/mypackage/mydags/mydag.py
@@ -1,13 +1,19 @@
 import datetime
+import time
 
-from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow import DAG, task
 
 from mypackage.lib.mylib import lib_method
 
 with DAG(
     dag_id="my_dag_name",
     start_date=datetime.datetime(2023, 4,1),
-    schedule="@daily",
+    schedule="@daily"
 ):
-    EmptyOperator(task_id="task")
+    @task(task_id=f"sleep")
+    def my_sleeping_function():
+        """This is a function that will run within the DAG execution"""
+        lib_method()
+        time.sleep(10)
+
+    sleeping_task = my_sleeping_function()
