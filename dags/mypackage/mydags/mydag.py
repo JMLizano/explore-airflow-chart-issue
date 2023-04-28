a69e7d5c2e9c26dc4fb400d4cf650628082e6276
chema <josemaria.lizano@lynxanalytics.com>
2023-04-28 19:36:50 +0800

    change start time

--- dags/mypackage/mydags/mydag.py
+++ dags/mypackage/mydags/mydag.py
@@ -10,10 +10,17 @@ with DAG(
     start_date=datetime.datetime(2023, 4,1),
     schedule="@daily"
 ) as dag:
+    
     @dag.task(task_id=f"sleep")
     def my_sleeping_function():
-        """This is a function that will run within the DAG execution"""
         lib_method()
         time.sleep(10)
+    
+    @dag.task(task_id=f"second_task")
+    def second_function():
+        print("Failed in previous task")
 
     sleeping_task = my_sleeping_function()
+    second_task = second_function()
+
+    sleeping_task > second_task
